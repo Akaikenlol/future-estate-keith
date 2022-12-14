13385342e91dadedef8cba158e0a7bcb08298f7a
SaiSeng18 <saisengmongli14@gmail.com>
2022-12-14 18:39:42 +0630

    finishing rent page, retouch login and signup page with animated gradient

--- public/pages/Home/index.js
+++ public/pages/Home/index.js
@@ -1,29 +1,29 @@
-// const scrollR = ScrollReveal({
-// 	opacity: 0,
-// 	distance: "20px",
-// 	duration: 1000,
-// });
+const scrollR = ScrollReveal({
+	opacity: 0,
+	distance: "20px",
+	duration: 1000,
+});
 
-// scrollR.reveal(`.popular-card, .blog-card`, {
-// 	origin: "left",
-// 	interval: 300,
-// 	delay: 300,
-// 	reset: true,
-// 	easing: "ease-out",
-// });
+scrollR.reveal(`.popular-card, .blog-card`, {
+	origin: "left",
+	interval: 300,
+	delay: 300,
+	reset: true,
+	easing: "ease-out",
+});
 
-// scrollR.reveal(`.service-img`, {
-// 	origin: "left",
-// 	distance: "100px",
-// 	duration: 700,
-// 	reset: true,
-// 	easing: "ease-in-out",
-// });
+scrollR.reveal(`.service-img`, {
+	origin: "left",
+	distance: "100px",
+	duration: 700,
+	reset: true,
+	easing: "ease-in-out",
+});
 
-// scrollR.reveal(`.service-content`, {
-// 	origin: "right",
-// 	distance: "100px",
-// 	duration: 700,
-// 	reset: true,
-// 	easing: "ease-in-out",
-// });
+scrollR.reveal(`.service-content`, {
+	origin: "right",
+	distance: "100px",
+	duration: 700,
+	reset: true,
+	easing: "ease-in-out",
+});
